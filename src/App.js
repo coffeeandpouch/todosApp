11c8df8ee090d966a6f5c19ca0b9c737e9f1b5bd
coffeeandpouch <sameinsidler@me.com>
2021-04-07 14:28:17 -0500

    commits changes in App for new folders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Todo from "./components/Todo";
-import TodoForm from "./components/TodoForm";
+import Todo from "./components/Todo/Todo";
+import TodoForm from "./components/TodoForm/TodoForm";
 
 import "./App.css";
 
