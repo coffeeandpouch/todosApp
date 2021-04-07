ac66344d761fa54fffdec595dfb7196cc58390ce
coffeeandpouch <sameinsidler@me.com>
2021-04-07 12:46:27 -0500

    commits changes for css and functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import Todo from "./components/Todo";
 
 import "./App.css";
@@ -27,14 +26,27 @@ class App extends React.Component {
     this.setState({ todos });
   };
 
+  deleteTodo = (e, index) => {
+    e.preventDefault();
+    e.stopPropagation();
+    let todos = this.state.todos.filter((todo, i) => parseInt(index, 10) !== i);
+    this.setState({ todos });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Todo Application</h1>
         <h2>{this.state.todos.length}</h2>
         <section className="todos">
-          {this.state.todos.map((todo) => (
-            <Todo todo={todo} toggleComplete={this.toggleComplete} />
+          {this.state.todos.map((todo, index) => (
+            <Todo
+              key={index}
+              index={index}
+              todo={todo}
+              toggleComplete={this.toggleComplete}
+              deleteTodo={this.deleteTodo}
+            />
           ))}
         </section>
       </div>
