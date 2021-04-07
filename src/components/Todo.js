a85a0d18e412690f3241505390cd1704d2e5404a
coffeeandpouch <sameinsidler@me.com>
2021-04-07 12:45:02 -0500

    commits adds for css functions and components

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,8 +2,14 @@ import React from "react";
 
 export default function Todo(props) {
   return (
-    <div className={props.todo.completed ? "Todo complete" : "Todo incomplete"}>
+    <div
+      className={props.todo.completed ? "Todo complete" : "Todo incomplete"}
+      onClick={(e) => props.toggleComplete(props.index)}
+    >
       <p>{props.todo.title}</p>
+      <a href="/deleteTodo" className="del" onClick={(e) => e.preventDefault()}>
+        X
+      </a>
     </div>
   );
 }
